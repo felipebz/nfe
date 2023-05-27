22b4e348349319932048dae7b0e455228b6cbc02
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2023-05-26 22:54:00 -0300

    Adicionada clases básicas para a integração de NF3e

---
+++ src/main/java/com/fincatto/documentofiscal/nf3e/Nf3eConfig.java
@@ -0,0 +1,10 @@
+package com.fincatto.documentofiscal.nf3e;
+
+import com.fincatto.documentofiscal.DFConfig;
+import com.fincatto.documentofiscal.cte300.classes.CTTipoEmissao;
+
+public abstract class Nf3eConfig extends DFConfig {
+
+    public static final String NAMESPACE = "http://www.portalfiscal.inf.br/nf3e";
+
+}
